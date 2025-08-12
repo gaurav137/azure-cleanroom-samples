6235d6b892388af5215219874cc905e62d479ab3
Gaurav Sinha <gsinha@microsoft.com>
2025-08-12 10:40:52 +0530

    Update

--- big-data/scripts/consortium/start-consortium.ps1
+++ big-data/scripts/consortium/start-consortium.ps1
@@ -11,7 +11,10 @@ param(
     [string]$environmentConfig = "$privateDir/$resourceGroup.generated.json",
     [string]$ccfProviderClient = "azure-cleanroom-samples-ccf-provider",
     [string]$cgsClient = "azure-cleanroom-samples-governance-client-$persona",
-    [string]$ccfEndpoint = "$publicDir/ccfEndpoint.json"
+    [string]$ccfEndpoint = "$publicDir/ccfEndpoint.json",
+
+    [string]$repo = "cleanroomemuprregistry.azurecr.io",
+    [string]$tag = "16749412789"
 )
 
 #https://learn.microsoft.com/en-us/powershell/scripting/learn/experimental-features?view=powershell-7.4#psnativecommanderroractionpreference
@@ -46,8 +49,7 @@ $subscriptionId = az account show --query id --output tsv
 # Register the ACI RP so that confidential container usage is enabled in the subscription.
 $aciRpName = "Microsoft.ContainerInstance"
 $aciRpRegistration = (az provider show -n $aciRpName --query registrationState --output tsv)
-if ($aciRpRegistration -ne "Registered")
-{
+if ($aciRpRegistration -ne "Registered") {
     Write-Log Verbose `
         "$aciRpName provider is not registered on the subscription. Registering provider (this can take a while)..."
     az provider register --namespace $aciRpName --wait
@@ -59,36 +61,33 @@ $ccf = & {
     # Disable $PSNativeCommandUseErrorActionPreference for this scriptblock
     $PSNativeCommandUseErrorActionPreference = $false
     return (az cleanroom ccf network show `
-        --name $ccfName `
-        --provider-config $privateDir/ccfProviderConfig.json `
-        --provider-client $ccfProviderClient | ConvertFrom-Json)
+            --name $ccfName `
+            --provider-config $privateDir/ccfProviderConfig.json `
+            --provider-client $ccfProviderClient | ConvertFrom-Json)
 }
 
 $ccfOperator = "ccfoperator"
 
-if ($null -eq $ccf)
-{
+if ($null -eq $ccf) {
     Write-Log OperationStarted `
         "Creating consortium '$ccfName' in resource group '$resourceGroup'..."
 
     # Generate ccf-operator identity and encryption public-private key pair.
-    $ccfOperatorMemberCert = $secretDir + "/"+ $ccfOperator + "_cert.pem"
-    $ccfOperatorEncryptionCert = $secretDir + "/"+ $ccfOperator + "_enc_pubk.pem"
+    $ccfOperatorMemberCert = $secretDir + "/" + $ccfOperator + "_cert.pem"
+    $ccfOperatorEncryptionCert = $secretDir + "/" + $ccfOperator + "_enc_pubk.pem"
 
     if ((Test-Path -Path $ccfOperatorMemberCert) -or
-        (Test-Path -Path $ccfOperatorEncryptionCert))
-    {
+        (Test-Path -Path $ccfOperatorEncryptionCert)) {
         Write-Log Warning `
             "Identity and/or encryption key pairs for '$ccfOperator' already exist."
     }
-    else
-    {
+    else {
         Write-Log Verbose `
             "Generating identity and encryption key pairs for '$ccfOperator' in '$secretDir'..."
         az cleanroom governance member keygenerator-sh | bash -s -- --gen-enc-key --name $ccfOperator --out $secretDir
     }
 
-    $memberCert = $secretDir + "/"+ $persona + "_cert.pem"
+    $memberCert = $secretDir + "/" + $persona + "_cert.pem"
 
     @"
 [{
@@ -110,21 +109,20 @@ if ($null -eq $ccf)
     Write-Log Verbose `
         "Creating CCF network '$ccfName' in '$resourceGroup'..."
     $ccf = (az cleanroom ccf network create `
-        --name $ccfName `
-        --node-count 1 `
-        --node-log-level "Info" `
-        --security-policy-creation-option "allow-all" `
-        --infra-type 'caci' `
-        --members $privateDir/ccfMembers.json `
-        --provider-config $privateDir/ccfProviderConfig.json `
-        --provider-client $ccfProviderClient | ConvertFrom-Json)
+            --name $ccfName `
+            --node-count 1 `
+            --node-log-level "Info" `
+            --security-policy-creation-option "allow-all" `
+            --infra-type 'caci' `
+            --members $privateDir/ccfMembers.json `
+            --provider-config $privateDir/ccfProviderConfig.json `
+            --provider-client $ccfProviderClient | ConvertFrom-Json)
     $ccfUri = $ccf.endpoint
 
     Write-Log OperationCompleted `
         "Created CCF network '$ccfName' ('$ccfUri')."
 }
-else
-{
+else {
     $ccfUri = $ccf.endpoint
     Write-Log Warning `
         "Connected to existing CCF network '$ccfName' ('$ccfUri')."
@@ -136,18 +134,24 @@ $serviceCertStr = $response.service_certificate.TrimEnd("`n")
 $serviceCert = "${ccfName}_service_cert.pem"
 $serviceCertStr | Out-File "$publicDir/$serviceCert"
 
+#
+# Use AZCLI_ overrides till latest images are available in mcr.microsoft.com.
+#
+$envVarsClientDeploy = @{
+    "AZCLI_CGS_CLIENT_IMAGE" = "$repo/cgs-client:$tag"
+    "AZCLI_CGS_UI_IMAGE"     = "$repo/cgs-ui:$tag"
+}
+
 & {
     # Deploy client-side containers to interact with the governance service as ccf-operator
     # and accept the invitation.
     $ccfOperatorClient = "azure-cleanroom-samples-governance-client-$ccfOperator"
     Write-Log Verbose `
         "Deploying governance client '$ccfOperatorClient'..."
-    az cleanroom governance client deploy `
-        --ccf-endpoint $ccfUri `
-        --signing-cert "$secretDir/$($ccfOperator)_cert.pem" `
-        --signing-key "$secretDir/$($ccfOperator)_privk.pem" `
-        --service-cert "$publicDir/$serviceCert" `
-        --name $ccfOperatorClient
+    Start-Process az `
+        -ArgumentList "cleanroom governance client deploy --ccf-endpoint $ccfUri --signing-cert $secretDir/$($ccfOperator)_cert.pem --signing-key $secretDir/$($ccfOperator)_privk.pem --service-cert $publicDir/$serviceCert --name $ccfOperatorClient" `
+        -Environment $envVarsClientDeploy  `
+        -Wait
     az cleanroom governance member activate --governance-client $ccfOperatorClient
 
     # Configure the ccf provider client for the operator and open the network.
@@ -181,27 +185,36 @@ $serviceCertStr | Out-File "$publicDir/$serviceCert"
     # and accept the invitation.
     Write-Log Verbose `
         "Deploying governance client '$cgsClient'..."
-    az cleanroom governance client deploy `
-        --ccf-endpoint $ccfUri `
-        --signing-cert "$secretDir/$($persona)_cert.pem" `
-        --signing-key "$secretDir/$($persona)_privk.pem" `
-        --service-cert "$publicDir/$serviceCert" `
-        --name $cgsClient
+    Start-Process az `
+        -ArgumentList "cleanroom governance client deploy --ccf-endpoint $ccfUri --signing-cert $secretDir/$($persona)_cert.pem --signing-key $secretDir/$($persona)_privk.pem --service-cert $publicDir/$serviceCert --name $cgsClient" `
+        -Environment $envVarsClientDeploy `
+        -Wait
     az cleanroom governance member activate --governance-client $cgsClient
 
     Write-Log OperationCompleted `
         "Joined consortium '$ccfUri' and deployed CGS client '$cgsClient'."
 }
 
+#
+# Use AZCLI_ overrides till latest images are available in mcr.microsoft.com.
+#
+$envVarsServiceDeploy = @{
+    "AZCLI_CGS_JSAPP_IMAGE"        = "$repo/cgs-js-app:$tag"
+    "AZCLI_CGS_CONSTITUTION_IMAGE" = "$repo/cgs-constitution:$tag"
+}
+
 # Deploy governance service on the CCF instance.
 Write-Log Verbose `
     "Deploying Clean Room Governance Service to '$ccfName'..."
-az cleanroom governance service deploy --governance-client $cgsClient
+Start-Process az `
+    -ArgumentList "cleanroom governance service deploy --governance-client $cgsClient" `
+    -Environment $envVarsServiceDeploy `
+    -Wait
 
 # Share the CCF endpoint details.
 $result = @{
-    name = $ccfName
-    url = $ccfUri
+    name        = $ccfName
+    url         = $ccfUri
     serviceCert = $serviceCert
 }
 $result | ConvertTo-Json -Depth 100 | Out-File "$ccfEndpoint"
