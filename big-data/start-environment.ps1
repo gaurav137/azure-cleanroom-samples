9e9a4a3c2097bd2c2987fb8aa350e77aa8a73a32
Gaurav Sinha <gsinha@microsoft.com>
2025-08-20 13:18:48 +0530

    Update

--- big-data/start-environment.ps1
+++ big-data/start-environment.ps1
@@ -19,7 +19,7 @@ param(
     [switch]$shareCredentials,
 
     [string]$repo = "cleanroomemuprregistry.azurecr.io",
-    [string]$tag = "17069141732"
+    [string]$tag = "17090450383"
 )
 
 #https://learn.microsoft.com/en-us/powershell/scripting/learn/experimental-features?view=powershell-7.4#psnativecommanderroractionpreference
