b2130a095b213149817fa17a0ede9c2494158252
Gaurav Sinha <gsinha@microsoft.com>
2025-08-30 13:23:17 +0530

    Update

--- big-data/scripts/common/ps-profile.ps1
+++ big-data/scripts/common/ps-profile.ps1
@@ -1,5 +1,5 @@
 function Prompt {
-    "PS [" + $env:PERSONA + "][" + $env:RESOURCE_GROUP + "] " + (Get-Location) + "> "
+    "PS [" + $env:PERSONA + "][" + $env:RESOURCE_GROUP + "][" + $env:DEMO + "] " + (Get-Location) + "> "
 }
 
 Set-PSReadLineOption -PredictionSource History
